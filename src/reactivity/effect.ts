04514fd8b52fb360318fe994857c6cc92df20b7e
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-01-11 22:30:40 +0800

    实现 effect 的 scheduler 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,8 +1,10 @@
 
 class ReactiveEffect {
   private _fn: any
-  constructor(fn) {
+  public scheduler: Function | undefined;
+  constructor(fn, scheduler?: Function) {
     this._fn = fn
+    this.scheduler = scheduler
   }
 
   run() {
@@ -41,13 +43,21 @@ export function trigger(target, key) {
   const depsMap = targetMap.get(target)
   const dep = depsMap.get(key)
   for (const effect of dep) {
-    effect.run()
+    if (typeof effect.scheduler === 'function') {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
 }
 
+type effectOptions = {
+  scheduler?: Function;
+};
+
 let activeEffect
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn, options:effectOptions = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
 
   _effect.run()
 
