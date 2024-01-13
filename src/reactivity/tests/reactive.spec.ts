3e4fee3e595aae119dbdcfa433d89bd628e763d2
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-01-13 22:29:59 +0800

    实现 isProxy

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,16 +1,14 @@
-import { isReactive, reactive } from '../reactive';
-
-describe('reactive', () => {
-  
-  it('happy test', () => {
-    const original = { foo: 1 }
-    const observed = reactive({ foo: 1 })
-    expect(observed).not.toBe(original)
-    expect(observed.foo).toBe(1)
-
-    expect(isReactive(observed)).toBe(true)
-    expect(isReactive(original)).toBe(false)
-  })
+import { isReactive, reactive, isProxy } from "../reactive";
+describe("reactive", () => {
+  it("happy path", () => {
+    const original = { foo: 1 };
+    const observed = reactive(original);
+    expect(observed).not.toBe(original);
+    expect(observed.foo).toBe(1);
+    expect(isReactive(observed)).toBe(true);
+    expect(isReactive(original)).toBe(false);
+    expect(isProxy(observed)).toBe(true);
+  });
 
   test("nested reactives", () => {
     const original = {
@@ -20,9 +18,8 @@ describe('reactive', () => {
       array: [{ bar: 2 }],
     };
     const observed = reactive(original);
-    // 我们期望 observed.nested 也是一个 响应式对象
     expect(isReactive(observed.nested)).toBe(true);
     expect(isReactive(observed.array)).toBe(true);
     expect(isReactive(observed.array[0])).toBe(true);
   });
-})
+});
