b33f1f9773557a7a0bbea5059c4468befef07e4f
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-01-13 15:08:58 +0800

    readonly-1

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -14,3 +14,18 @@ export function reactive(target) {
     }
   })
 }
+
+
+// 1、readonly 的对象不会被 track，
+// 2、readonly 的属性值不可更改，set 中直接返回 true 即可
+export function readonly(target) {
+  return new Proxy(target, {
+    get(target, key) {
+      const res = Reflect.get(target, key)
+      return res
+    },
+    set() {
+      return true
+    }
+  })
+}
