9c2cf30c2c58ebefedc021d9466a7d52c2c775b2
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-01-13 21:05:27 +0800

    实现 isReactive 和 isReadonly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,4 +1,5 @@
 import { track, trigger } from "./effect"
+import { ReactiveFlags } from "./reactive";
 
 const get = createGetter()
 const readonlyGet = createGetter(true)
@@ -6,6 +7,12 @@ const set = createSetter()
 
 function createGetter(isReadonly = false) {
   return function get(target, key, receiver) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
+    
     const res = Reflect.get(target, key, receiver)
     // readonly 的对象不会被 track
     if (!isReadonly) {
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,10 @@
 import { mutableHandlers, readonlyHandlers } from './baseHandlers';
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+}
+
 function createActiveObject(raw, baseHandlers) {
   return new Proxy(raw, baseHandlers)
 }
@@ -12,4 +17,13 @@ export function reactive(raw) {
 // 2、readonly 的属性值不可更改，set 中直接返回 true 即可
 export function readonly(raw) {
   return createActiveObject(raw, readonlyHandlers)
+}
+export function isReactive(value) {
+  // 如果 value 是一个响应式对象， 在 value 身上进行取值，就会触发 getter，在 getter 中进行了判断，返回了 true
+  // 如果 value 不是一个响应式对象，value[ReactiveFlags.IS_REACTIVE] 的值为 undefined， 转成 boolean 也就是 false
+  return !!value[ReactiveFlags.IS_REACTIVE];
+}
+
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY];
 }
