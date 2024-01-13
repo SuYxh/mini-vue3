05123a0ad28f5b3d1d0e258257e5d4ec25c72bc4
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-01-13 15:17:12 +0800

    readonly-2-提取公共部分

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,29 +1,37 @@
 import { track, trigger } from "./effect"
 
-export function reactive(target) {
-  return new Proxy(target, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
+function createGetter(isReadonly = false) {
+  return function get(target, key, receiver) {
+    const res = Reflect.get(target, key, receiver)
+    if (!isReadonly) {
       track(target, key)
-      return res
-    },
-    set(target, key, val) {
-      const res = Reflect.set(target, key, val)
-      trigger(target,key)
-      return res
     }
+    return res
+  }
+}
+
+
+function createSetter() {
+  return function set(target, key, val, receiver) {
+    const res = Reflect.set(target, key, val, receiver)
+    trigger(target, key)
+    return res
+  }
+}
+
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get: createGetter(),
+    set: createSetter(),
   })
 }
 
 
 // 1、readonly 的对象不会被 track，
 // 2、readonly 的属性值不可更改，set 中直接返回 true 即可
-export function readonly(target) {
-  return new Proxy(target, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
-      return res
-    },
+export function readonly(raw) {
+  return new Proxy(raw, {
+    get: createGetter(true),
     set() {
       return true
     }
