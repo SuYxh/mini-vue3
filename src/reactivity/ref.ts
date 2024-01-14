c381ad43375b31b229f347e057b95526a6df265c
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-01-14 10:18:14 +0800

    实现 proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -59,3 +59,22 @@ export function isRef(ref) {
 export function unRef(ref) {
   return isRef(ref) ? ref.value : ref;
 }
+
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+
+    set(target, key, value) {
+      // set 分为两种情况：
+      // 如果原来的值是 ref，并且新的值不是 ref，那么就去更新原来的 ref.value = newValue
+      // 第二种情况就是原来的值是 ref，newValue 也是一个 ref，那么就直接替换就 OK 了
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
+}
